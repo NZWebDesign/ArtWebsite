3265b42068890b83b4e911b2c5f1869c6dda5432
NZWebDesign <65640337+NZWebDesign@users.noreply.github.com>
2020-11-09 18:12:46 +1300

    update

--- app.js
+++ app.js
@@ -8,3 +8,4 @@ timeLine.fromTo(".bigText", {opacity:0}, {opacity: 1, duration:1}, '-=1');
 timeLine.fromTo(".smallText", {opacity:0}, {opacity: 1, duration:1}, '-=1');
 timeLine.fromTo(".hiddenPictures", {opacity:0}, {opacity: 1, duration:1}, '-=1');
 timeLine.fromTo(".footer", {opacity:0}, {opacity: 1, duration:1}, '-=1');
+/** */
